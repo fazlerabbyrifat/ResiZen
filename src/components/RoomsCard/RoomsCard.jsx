8b4ab94799d8d3e0f976cde80f1a2fba83bc9dba
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-08-27 20:40:53 +0600

    slider is added to the rooms card image

--- src/components/RoomsCard/RoomsCard.jsx
+++ src/components/RoomsCard/RoomsCard.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineKingBed } from "react-icons/md";
 import { BiBath } from "react-icons/bi";
+import { ArrowLeftIcon, ArrowRightIcon, MapPinIcon, PhoneIcon } from "@heroicons/react/24/solid";
 
 const RoomsCard = ({ room }) => {
   const {
@@ -16,19 +17,44 @@ const RoomsCard = ({ room }) => {
     price,
     contact,
   } = room;
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleNextClick = () => {
+    const nextIndex = (currentImageIndex + 1) % images.length;
+    setCurrentImageIndex(nextIndex);
+  };
+
+  const handlePrevClick = () => {
+    const prevIndex = (currentImageIndex - 1 + images.length) % images.length;
+    setCurrentImageIndex(prevIndex);
+  };
+
   return (
     <div className="rounded-lg overflow-hidden shadow-lg hover:shadow-xl">
-      <div className="relative">
+      <div className="w-full h-96 relative">
         <Image
-          className="w-full h-96 object-cover transition-shadow duration-300 transform hover:shadow-md hover:scale-105"
-          src={images[0]}
+          className="w-full h-full object-cover transition-shadow duration-300 transform hover:shadow-md hover:scale-105"
+          src={images[currentImageIndex]}
           alt={name}
           width={350}
           height={350}
-        ></Image>
+        />
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-center opacity-0 hover:opacity-100 transition-opacity duration-300">
           <p className="text-lg font-semibold">{name}</p>
         </div>
+        <button
+          onClick={handlePrevClick}
+          className="absolute top-1/2 left-0 transform -translate-y-1/2 text-black px-4 py-2 rounded-full m-2"
+        >
+          <ArrowLeftIcon className="h-5 w-5" />
+        </button>
+        <button
+          onClick={handleNextClick}
+          className="absolute top-1/2 right-0 transform -translate-y-1/2 text-black px-4 py-2 rounded-full m-2"
+        >
+          <ArrowRightIcon className="h-5 w-5" />
+        </button>
       </div>
       <div className="p-4">
         <h2 className="text-xl font-semibold mb-2">{name}</h2>
@@ -54,13 +80,22 @@ const RoomsCard = ({ room }) => {
           </p>
         ))}
         </div>
-        <p className="text-sm font-normal mb-2">{address}</p>
-        <p className="text-sm font-normal mb-2">{contact}</p>
+        <p className="text-sm font-normal mb-2 flex items-center">
+          <span>
+            <MapPinIcon className="w-3 h-3 mr-2" />
+          </span>
+          {address}
+          </p>
+        <p className="text-sm font-normal mb-2 flex items-center">
+          <span>
+            <PhoneIcon className="w-3 h-3 mr-2" />
+          </span>
+          {contact}</p>
         <div className="flex mt-4">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2">
+          <button className="bg-[#ddeaf6] text-[#235784] px-4 py-2 rounded-full mr-2">
             Book Now
           </button>
-          <button className="bg-gray-300 text-gray-700 px-4 py-2 rounded-full">
+          <button className="bg-[#235784] text-white px-4 py-2 rounded-full">
             Details
           </button>
         </div>
